49c9c5641aa33ef60866bd7ad637f3e3f26efccc
Aliaksandr Kleshchukevich <aliaksandr.kleshchukevich@gojuno.com>
2020-04-29 22:46:51 +0300

    simplify pubsub

--- src/lib/pubsub.js
+++ src/lib/pubsub.js
@@ -6,24 +6,20 @@ class PubSubClass {
 
   subscribe(event, callback) {
 
-    const self = this;
-  
-    if(!self.events.hasOwnProperty(event)) {
-      self.events[event] = [];
+    if(!this.events.hasOwnProperty(event)) {
+      this.events[event] = [];
     }
   
-    return self.events[event].push(callback);
+    return this.events[event].push(callback);
   }
 
   publish(event, data = {}) {
-
-    const self = this;
   
-    if(!self.events.hasOwnProperty(event)) {
+    if(!this.events.hasOwnProperty(event)) {
       return [];
     }
   
-    return self.events[event].map(callback => callback(data));
+    return this.events[event].map(callback => callback(data));
   }
 }
 
